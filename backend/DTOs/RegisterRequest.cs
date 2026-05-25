60759954fcc5afac47e6dcf040951d9d11724039
Giuseppe Aprile <aprilegiuseppe@gmail.com>
2026-05-25 20:54:36 +0200

    Add: RegisterRequest DTO

---
+++ backend/DTOs/RegisterRequest.cs
@@ -0,0 +1,10 @@
+namespace NuovaPrevidenza.API.DTOs
+{
+    public class RegisterRequest
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
